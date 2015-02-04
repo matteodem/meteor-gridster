fc89d18b48f69ba0a067a9a71d3028c601c4ea54
Matteo De Micheli <mat.demicheli@gmail.com>
2015-02-04 13:55:29 +0100

    First commit

---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  name: 'matteodem:gridster',
+  version: '0.0.1',
+  summary: 'gridster.js repackaged for Meteor',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.1');
+  api.use('jquery', 'client');
+  api.addFiles('lib/jquery.gridster.js', 'client');
+  api.addFiles('lib/jquery.gridster.css', 'client');
+});
